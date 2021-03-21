48406df4c574dd5457a7edde7cf29b4067fb9ddc
Joris <you@example.com>
2021-03-21 18:26:32 +0100

    hero + nav

---
+++ index.js
@@ -0,0 +1,27 @@
+const burgerMenu = document.getElementById('menu-burger')
+const navbar = document.getElementById('navbar')
+
+function toggleMenuBurger() {
+  document.body.classList.toggle('burger-active')
+
+  if (document.body.classList.contains('burger-active')) {
+    navbar.classList.remove('nav-overlay')
+  } else {
+    navbar.classList.add('nav-overlay')
+  }
+}
+
+burgerMenu.onclick = toggleMenuBurger
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Escape') {
+    toggleMenuBurger()
+  }
+});
+
+document.body.onscroll = function () {
+  if (window.scrollY <= 80) {
+    navbar.classList.remove('nav-overlay')
+  } else if (window.scrollY > 80 && !navbar.classList.contains('nav-overlay')) {
+    navbar.classList.add('nav-overlay')
+  }
+}
